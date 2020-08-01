0e1bc683b7234a210ee56aeae921767664a87f88
HallexCosta <hallex.costa@hotmail.com>
2020-08-01 18:13:35 -0300

    Finish interface discord

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-import GlobalStyles from './styles/GlobalStyles'
 import Layout from './components/Layout'
+import GlobalStyle from './styles/GlobalStyles'
 
 function App() {
   return (
     <>
       <Layout />
 
-      <GlobalStyles />
+      <GlobalStyle />
     </>
   )
 }
